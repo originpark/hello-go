47eb9fe34b4d53409ae8b31570847d90ea082bf9
Jonas Nachtwald <wildhearts86@gmail.com>
2025-07-13 16:30:01 +0800

    feat: add a error handle system

--- hello.go
+++ hello.go
@@ -1,8 +1,14 @@
 package hello_go
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name is empty")
+	}
 	message := fmt.Sprintf("hello, %v, weclome!", name)
-	return message
+	return message, nil
 }
